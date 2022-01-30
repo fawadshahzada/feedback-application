b38517eac7f58f03cacd84f01545d2607f0a08fa
Fawad Shahzada <fawadshahzada@gmail.com>
2022-01-30 21:42:23 +0500

    minor changes

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,38 +1,16 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackData from "./data/FeedbackData";
 function App() {
-    const title = 'Blog post'
-    const body = 'This is my blog post...'
-    const comments = [
-        {id: 1, text:'comment one'},
-        {id: 2, text:'comment two'},
-        {id: 3, text:'comment three'},
-        {id: 4, text:'comment four'}
-    ]
-    const loading = false
-    const showComments =  true
-
-    if(loading) return <h1>Loading...</h1>
-    const commentBlock = (
-    <div className="comments">
-        <h3>Comments ({comments.length})</h3>
-        <ul>
-            {comments.map((comment, index) =>(
-
-            <li key={index}>{comment.text}</li>
-            ))}
-        </ul>   
-    </div>
-    )
-    return (
-
-        <div className='container'>
-             <h1>{title.toUpperCase()}</h1>
-             <p>{body}</p>
-
-             {showComments && commentBlock }
-              
-        </div>
-       
-       
-    )
+    const [feedback, setFeedback ] = useState(FeedbackData)
+   return (
+    <>
+        <Header />
+       <div>
+           <FeedbackList feedback = {feedback} />
+       </div>
+    </>
+   )
 }
 export default App;
