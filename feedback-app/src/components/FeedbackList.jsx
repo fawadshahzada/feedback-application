e23889df517fdaa6c9cffd2cf6a3cc5e62a05baa
Fawad Shahzada <fawadshahzada@gmail.com>
2022-02-01 22:58:14 +0500

    card jsx added

--- feedback-app/src/components/FeedbackList.jsx
+++ feedback-app/src/components/FeedbackList.jsx
@@ -5,8 +5,9 @@ function feedbackList( {feedback} ) {
   }
   return (
   <div className="feedback-list">
+    
     {feedback.map((item) => {
-      return <FeedbackItem key={item.id} item={item}/>
+      return <FeedbackItem item={item}/>
     })}
   </div>
   )
